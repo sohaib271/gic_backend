5b235bb4674475773ec61f059f551d90c5fbbf4e
sohaib271 <sohaibsheikh6299@gmail.com>
2026-03-09 21:56:46 +0500

    App module updated to read env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { DepartmentModule } from './department/department.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,9 +9,13 @@ import { ClassModule } from './class/class.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/college-db',
-    ),
+   MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL') || 'mongodb://localhost:27017/college-db',
+      }),
+    }),
     UserModule,
     DepartmentModule,
     AuthModule,
