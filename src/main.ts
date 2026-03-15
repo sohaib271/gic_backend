41de51faf9a8321310c7502c0a49c1c1fcc70d4a
sohaib271 <sohaibsheikh6299@gmail.com>
2026-03-15 21:18:36 +0500

    fhjew

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dns from "dns"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   dns.setDefaultResultOrder("ipv4first");
 
    app.enableCors({
     origin: '*',
