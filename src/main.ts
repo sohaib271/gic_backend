c6696e4a667c9495c849c7aba8704baff72db34c
Murtza Zabair <murtzazabair@gmail.com>
2026-04-10 14:03:46 +0500

    add the swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.enableCors({
+  app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('GIC Backend API')
+    .setDescription('API documentation for the GIC backend project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
