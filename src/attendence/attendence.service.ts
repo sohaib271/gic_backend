8de15f4478579a3cfdf749ecde1f02fa5e2eb424
sohaib271 <sohaibsheikh6299@gmail.com>
2026-05-09 15:38:55 +0500

    Attendance errors fixed

--- src/attendence/attendence.service.ts
+++ src/attendence/attendence.service.ts
@@ -7,7 +7,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model,Types } from 'mongoose';
 import { Class, ClassDocument } from 'src/class/schema/class.schema';
 import { Attendance, AttendenceDocument } from './schema/attendence.schema';
 import { User, UserDocument } from 'src/user/schema/user.schema';
